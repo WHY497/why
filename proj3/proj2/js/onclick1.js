ce4cfe7abf50bc00eb0fa3a0ead46cf84058da28
WHY497 <529044029@qq.com>
2020-07-08 17:23:07 +0800

    网页

---
+++ proj3/proj2/js/onclick1.js
@@ -0,0 +1,23 @@
+document.getElementById("sk1").onclick = function () {
+    self.location='travalto.html' 
+}
+document.getElementById("sk2").onclick = function () {
+    self.location = 'Untitled1.html';
+
+}
+document.getElementById("sk3").onclick = function () {
+    self.location = "page3.html";
+
+}
+document.getElementById("sk4").onclick = function () {
+    self.location = "page3.html";
+
+}
+document.getElementById("sk5").onclick = function () {
+    self.location = 'shopping.html';
+
+}
+document.getElementById("sk6").onclick = function () {
+    self.location = "page3.html";
+
+}
